1844783f148488de7200c322df667d8f995f6eb7
Golodhrim <holidayninjastaff@gmail.com>
2022-10-04 17:14:41 -0400

    reset api endpoint

--- src/actions/post.js
+++ src/actions/post.js
@@ -2,216 +2,234 @@ import axios from 'axios';
 import { setAlert } from './alert';
 
 import {
-  GET_POST,
-  GET_POSTS,
-  CREATE_POST,
-  DELETE_POST,
-  POST_ERROR,
-  UPDATE_LIKES,
-  ADD_COMMENT,
-  REMOVE_COMMENT,
+	GET_POST,
+	GET_POSTS,
+	CREATE_POST,
+	DELETE_POST,
+	POST_ERROR,
+	UPDATE_LIKES,
+	ADD_COMMENT,
+	REMOVE_COMMENT,
 } from './types';
+
+const baseApi = process.env.REACT_APP_RENDER_API;
 
 // Get a Post by Id
 
 export const getPost = (id) => async (dispatch) => {
-  try {
-    const res = await axios.get(`/api/posts/post/${id}`);
-    dispatch({
-      type: GET_POST,
-      payload: res.data,
-    });
-  } catch (error) {
-    console.log(error);
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	try {
+		const res = await axios.get(`${baseApi}/api/posts/post/${id}`);
+		dispatch({
+			type: GET_POST,
+			payload: res.data,
+		});
+	} catch (error) {
+		console.log(error);
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Get all Posts
 
 export const getPosts = () => async (dispatch) => {
-  try {
-    const config = {
-      header: {
-        'Content-type': 'application/json',
-      },
-    };
-    const body = '';
-    const res = await axios.get('/api/posts/all', body, config);
-    dispatch({
-      type: GET_POSTS,
-      payload: res.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	try {
+		const config = {
+			header: {
+				'Content-type': 'application/json',
+			},
+		};
+		const body = '';
+		const res = await axios.get(`${baseApi}/api/posts/all`, body, config);
+		dispatch({
+			type: GET_POSTS,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Create a post
 
 export const createPost = (formData) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = formData;
-    const res = await axios.post('/api/posts', body, config);
-    dispatch({
-      type: CREATE_POST,
-      payload: res.data,
-    });
-    dispatch(setAlert('Nice Post!', 'success'));
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-    dispatch(setAlert(error.response.data.msg, 'danger'));
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = formData;
+		const res = await axios.post(`${baseApi}/api/posts`, body, config);
+		dispatch({
+			type: CREATE_POST,
+			payload: res.data,
+		});
+		dispatch(setAlert('Nice Post!', 'success'));
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+		dispatch(setAlert(error.response.data.msg, 'danger'));
+	}
 };
 
 // Delete a post
 
 export const deletePost = (id) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.delete(`/api/posts/delete/${id}`, body, config);
-    dispatch({
-      type: DELETE_POST,
-      payload: id,
-    });
-    dispatch(setAlert(res.data.msg, 'success'));
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-    dispatch(setAlert(error.response.data.msg, 'danger'));
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.delete(
+			`${baseApi}/api/posts/delete/${id}`,
+			body,
+			config
+		);
+		dispatch({
+			type: DELETE_POST,
+			payload: id,
+		});
+		dispatch(setAlert(res.data.msg, 'success'));
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+		dispatch(setAlert(error.response.data.msg, 'danger'));
+	}
 };
 
 // Add a like to a post
 
 export const addLike = (id) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authrorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.patch(`/api/posts/like/${id}`, body, config);
-    dispatch({
-      type: UPDATE_LIKES,
-      payload: { id, likes: res.data },
-    });
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authrorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.patch(
+			`${baseApi}/api/posts/like/${id}`,
+			body,
+			config
+		);
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Removing a like from a post
 
 export const removeLike = (id) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authrorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.patch(`/api/posts/unlike/${id}`, body, config);
-    dispatch({
-      type: UPDATE_LIKES,
-      payload: { id, likes: res.data },
-    });
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authrorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.patch(
+			`${baseApi}/api/posts/unlike/${id}`,
+			body,
+			config
+		);
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Create a Comment on a Post
 
 export const createComment = (id, formData) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = formData;
-    const res = await axios.post(`/api/posts/comment/${id}`, body, config);
-    dispatch({
-      type: ADD_COMMENT,
-      payload: res.data,
-    });
-    dispatch(setAlert('Tasty Comment!', 'success'));
-  } catch (error) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-    dispatch(setAlert(error.response.data.msg, 'danger'));
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = formData;
+		const res = await axios.post(
+			`${baseApi}/api/posts/comment/${id}`,
+			body,
+			config
+		);
+		dispatch({
+			type: ADD_COMMENT,
+			payload: res.data,
+		});
+		dispatch(setAlert('Tasty Comment!', 'success'));
+	} catch (error) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+		dispatch(setAlert(error.response.data.msg, 'danger'));
+	}
 };
 
 // Delete a Comment on a Post
 
 export const removeComment = (postId, commentId) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.delete(
-      `/api/posts/comment/${postId}/${commentId}`,
-      body,
-      config
-    );
-    dispatch({
-      type: REMOVE_COMMENT,
-      payload: commentId,
-    });
-    dispatch(setAlert('Mind Changed Successfully...', 'success'));
-  } catch (error) {
-    console.log(error);
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-    dispatch(setAlert(error.response.data.msg, 'danger'));
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.delete(
+			`${baseApi}/api/posts/comment/${postId}/${commentId}`,
+			body,
+			config
+		);
+		dispatch({
+			type: REMOVE_COMMENT,
+			payload: commentId,
+		});
+		dispatch(setAlert('Mind Changed Successfully...', 'success'));
+	} catch (error) {
+		console.log(error);
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+		dispatch(setAlert(error.response.data.msg, 'danger'));
+	}
 };
--- src/actions/profile.js
+++ src/actions/profile.js
@@ -2,290 +2,298 @@ import axios from 'axios';
 import { setAlert } from './alert';
 
 import {
-  CLEAR_PROFILE,
-  GET_PROFILE,
-  GET_PROFILES,
-  GET_REPOS,
-  PROFILE_ERROR,
-  UPDATE_PROFILE,
+	CLEAR_PROFILE,
+	GET_PROFILE,
+	GET_PROFILES,
+	GET_REPOS,
+	PROFILE_ERROR,
+	UPDATE_PROFILE,
 } from './types';
+
+const baseApi = process.env.REACT_APP_RENDER_API;
 
 // Get a current profile
 
 export const getCurrentProfile = () => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      header: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.get('/api/profile/me', body, config);
-    dispatch({
-      type: GET_PROFILE,
-      payload: res.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			header: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.get(`${baseApi}/api/profile/me`, body, config);
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Get all Profiles
 
 export const getAllProfiles = () => async (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE });
-  try {
-    const config = {
-      header: {
-        'Content-type': 'application/json',
-      },
-    };
-    const body = '';
-    const res = await axios.get('/api/profiles', body, config);
-    dispatch({
-      type: GET_PROFILES,
-      payload: res.data,
-    });
-  } catch (error) {
-    console.log(error);
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	dispatch({ type: CLEAR_PROFILE });
+	try {
+		const config = {
+			header: {
+				'Content-type': 'application/json',
+			},
+		};
+		const body = '';
+		const res = await axios.get(`${baseApi}/api/profiles`, body, config);
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data,
+		});
+	} catch (error) {
+		console.log(error);
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Get Profile by ID
 
 export const getProfileById = (userid) => async (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE });
-  try {
-    const _id = userid;
-    const config = {
-      header: {
-        'Content-type': 'application/json',
-      },
-    };
-    const body = '';
-    const res = await axios.get(`/api/profileById/${_id}`, body, config);
-    dispatch({
-      type: GET_PROFILE,
-      payload: res.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	dispatch({ type: CLEAR_PROFILE });
+	try {
+		const _id = userid;
+		const config = {
+			header: {
+				'Content-type': 'application/json',
+			},
+		};
+		const body = '';
+		const res = await axios.get(
+			`${baseApi}/api/profileById/${_id}`,
+			body,
+			config
+		);
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Get GitHub Repos
 
 export const getGitHubRepos = (username) => async (dispatch) => {
-  try {
-    const config = {
-      header: {
-        'Content-type': 'application/json',
-      },
-    };
-    const body = '';
-    const res = await axios.get(
-      `/api/profile/github/${username}`,
-      body,
-      config
-    );
-    dispatch({
-      type: GET_REPOS,
-      payload: res.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data.msg, status: error.response.status },
-    });
-  }
+	try {
+		const config = {
+			header: {
+				'Content-type': 'application/json',
+			},
+		};
+		const body = '';
+		const res = await axios.get(
+			`${baseApi}/api/profile/github/${username}`,
+			body,
+			config
+		);
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data.msg, status: error.response.status },
+		});
+	}
 };
 
 // Create or Update a profile
 
-export const createProfile = (formData, history, edit = false) => async (
-  dispatch
-) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const res = await axios.post('/api/profile', formData, config);
+export const createProfile =
+	(formData, history, edit = false) =>
+	async (dispatch) => {
+		const token = localStorage.getItem('token');
+		try {
+			const config = {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: token,
+				},
+			};
+			const res = await axios.post(`${baseApi}/api/profile`, formData, config);
 
-    dispatch({
-      type: GET_PROFILE,
-      payload: res.data,
-    });
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			});
 
-    dispatch(setAlert(edit ? 'Deets Changed!' : 'Profile Commit!', 'success'));
-    if (!edit) {
-      history.push('/dashboard');
-    }
-  } catch (error) {
-    console.log(error.response.data);
-    dispatch(setAlert(error.response.data, 'danger'));
+			dispatch(
+				setAlert(edit ? 'Deets Changed!' : 'Profile Commit!', 'success')
+			);
+			if (!edit) {
+				history.push('/dashboard');
+			}
+		} catch (error) {
+			console.log(error.response.data);
+			dispatch(setAlert(error.response.data, 'danger'));
 
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data, status: error.response.status },
-    });
-  }
-};
+			dispatch({
+				type: PROFILE_ERROR,
+				payload: { msg: error.response.data, status: error.response.status },
+			});
+		}
+	};
 
 // Add Education to profile
 
 export const addEducation = (formData, history) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const res = await axios.patch(
-      '/api/profile/me/education',
-      formData,
-      config
-    );
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const res = await axios.patch(
+			`${baseApi}/api/profile/me/education`,
+			formData,
+			config
+		);
 
-    dispatch({
-      type: UPDATE_PROFILE,
-      payload: res.data,
-    });
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
 
-    dispatch(setAlert('Education Detected!', 'success'));
+		dispatch(setAlert('Education Detected!', 'success'));
 
-    history.push('/dashboard');
-  } catch (error) {
-    console.log(error.response.data);
-    dispatch(setAlert(error.response.data, 'danger'));
+		history.push('/dashboard');
+	} catch (error) {
+		console.log(error.response.data);
+		dispatch(setAlert(error.response.data, 'danger'));
 
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data, status: error.response.status },
-    });
-  }
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data, status: error.response.status },
+		});
+	}
 };
 
 // Add Experience to profile
 
 export const addExperience = (formData, history) => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const res = await axios.patch(
-      '/api/profile/me/experience',
-      formData,
-      config
-    );
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const res = await axios.patch(
+			`${baseApi}/api/profile/me/experience`,
+			formData,
+			config
+		);
 
-    dispatch({
-      type: UPDATE_PROFILE,
-      payload: res.data,
-    });
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
 
-    dispatch(setAlert('Experience Authenticated!', 'success'));
+		dispatch(setAlert('Experience Authenticated!', 'success'));
 
-    history.push('/dashboard');
-  } catch (error) {
-    console.log(error.response.data);
-    dispatch(setAlert(error.response.data, 'danger'));
+		history.push('/dashboard');
+	} catch (error) {
+		console.log(error.response.data);
+		dispatch(setAlert(error.response.data, 'danger'));
 
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.data, status: error.response.status },
-    });
-  }
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.data, status: error.response.status },
+		});
+	}
 };
 
 // Delete an experience
 
 export const deleteExperience = (id) => async (dispatch) => {
-  const _id = id;
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.post(
-      `/api/profile/me/experience/${_id}`,
-      body,
-      config
-    );
+	const _id = id;
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.post(
+			`${baseApi}/api/profile/me/experience/${_id}`,
+			body,
+			config
+		);
 
-    dispatch({
-      type: UPDATE_PROFILE,
-      payload: res.data,
-    });
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
 
-    dispatch(setAlert('Experience Removed', 'success'));
-  } catch (error) {
-    console.log(error);
-    dispatch(setAlert(error.message, 'danger'));
+		dispatch(setAlert('Experience Removed', 'success'));
+	} catch (error) {
+		console.log(error);
+		dispatch(setAlert(error.message, 'danger'));
 
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.message, status: error.response.status },
-    });
-  }
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.message, status: error.response.status },
+		});
+	}
 };
 // Delete education
 
 export const deleteEducation = (id) => async (dispatch) => {
-  const _id = id;
-  const token = localStorage.getItem('token');
-  try {
-    const config = {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: token,
-      },
-    };
-    const body = '';
-    const res = await axios.post(
-      `/api/profile/me/education/${_id}`,
-      body,
-      config
-    );
+	const _id = id;
+	const token = localStorage.getItem('token');
+	try {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		};
+		const body = '';
+		const res = await axios.post(
+			`${baseApi}/api/profile/me/education/${_id}`,
+			body,
+			config
+		);
 
-    dispatch({
-      type: UPDATE_PROFILE,
-      payload: res.data,
-    });
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
 
-    dispatch(setAlert('Education Removed', 'success'));
-  } catch (error) {
-    console.log(error);
-    dispatch(setAlert(error.message, 'danger'));
+		dispatch(setAlert('Education Removed', 'success'));
+	} catch (error) {
+		console.log(error);
+		dispatch(setAlert(error.message, 'danger'));
 
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.message, status: error.response.status },
-    });
-  }
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.message, status: error.response.status },
+		});
+	}
 };

e7783cde3b28cf898ea219a3c1299d4e4342fa19
Golodhrim <holidayninjastaff@gmail.com>
2022-10-04 22:31:52 -0400

    guest links show in nav until loading is complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,63 +5,65 @@ import PropTypes from 'prop-types';
 import { logout } from '../actions/auth';
 
 const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
-        </Link>
-      </li>
-      <li>
-        <Link onClick={logout} to='/login'>
-          <i className='fas fa-sign-out-alt' />{' '}
-          <span className='hide-sm'>Logout</span>
-        </Link>
-      </li>
-    </ul>
-  );
+	const authLinks = (
+		<ul>
+			<li>
+				<Link to='/profiles'>Developers</Link>
+			</li>
+			<li>
+				<Link to='/posts'>Posts</Link>
+			</li>
+			<li>
+				<Link to='/dashboard'>
+					<i className='fas fa-user' />{' '}
+					<span className='hide-sm'>Dashboard</span>
+				</Link>
+			</li>
+			<li>
+				<Link onClick={logout} to='/login'>
+					<i className='fas fa-sign-out-alt' />{' '}
+					<span className='hide-sm'>Logout</span>
+				</Link>
+			</li>
+		</ul>
+	);
 
-  const guestLinks = (
-    <ul>
-      <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
-        <Link to='/login'>Login</Link>
-      </li>
-    </ul>
-  );
+	const guestLinks = (
+		<ul>
+			<li>
+				<Link to='/profiles'>Developers</Link>
+			</li>
+			<li>
+				<Link to='/register'>Register</Link>
+			</li>
+			<li>
+				<Link to='/login'>Login</Link>
+			</li>
+		</ul>
+	);
 
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-code-branch'></i> SocialDevs
-        </Link>
-      </h1>
-      {!loading && (
-        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-      )}
-    </nav>
-  );
+	return (
+		<nav className='navbar bg-dark'>
+			<h1>
+				<Link to='/'>
+					<i className='fas fa-code-branch'></i> SocialDevs
+				</Link>
+			</h1>
+			{loading ? (
+				<Fragment>{guestLinks}</Fragment>
+			) : (
+				<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+			)}
+		</nav>
+	);
 };
 NavBar.propTypes = {
-  logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
+	logout: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
+	auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(NavBar);
